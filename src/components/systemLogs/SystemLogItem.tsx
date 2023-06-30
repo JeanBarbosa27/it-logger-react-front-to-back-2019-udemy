ea1be658ec832a0c73c8c108f0339d8af158b55d
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-06-30 08:34:52 +0100

    refact: improve log item appearence

--- src/components/systemLogs/SystemLogItem.tsx
+++ src/components/systemLogs/SystemLogItem.tsx
@@ -14,13 +14,27 @@ const SystemLogItem = ({ log }: ISystemLogItemParams) => {
       { dateStyle: 'medium', timeStyle: 'medium', timeZone: 'Europe/London' }
     ).format(time);
   }
+  const lastUpdatedOn = formatDate(log.date);
 
   return (
-    <>
-      <span>{log.message}</span>
-      <span>by {log.tech}</span>
-      <span>in {formatDate(log.date)}</span>
-    </>
+    <li>
+      <ul>
+        <li>
+          <a
+            href="#edit-log-model"
+            className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}
+          >{log.message}</a>
+        </li>
+        <li className="black-text children-space-1">
+          <span>ID #{log.id}</span>
+          <a href="#2">by {log.tech}</a>
+          <span className="grey-text">last updated on {lastUpdatedOn}</span>
+          <a href="#!" className="secondary-content grey-text">
+            <i className="material-icons delete-button">delete</i>
+          </a>
+        </li>
+      </ul>
+    </li>
   )
 }
 

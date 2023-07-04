c7dbe6ff500314b60714b9a6003f7b66de4361da
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-04 09:28:50 +0100

    feat: create and configure store

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+// Once createStore is deprecated, I didn't follow the class here, I followed the redux docs: https://redux.js.org/usage/configuring-your-store/
+
+import { configureStore } from '@reduxjs/toolkit';
+import systemLogReducer from './reducers/systemLogReducer';
+
+const store = configureStore({
+  reducer: {
+    systemLogReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store;

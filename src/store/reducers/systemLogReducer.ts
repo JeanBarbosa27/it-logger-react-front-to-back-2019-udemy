ae4ec14fb083ba66a053c91710f725e8bfacb4d7
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-05 08:31:45 +0100

    feat: add reducers and selectors

--- src/store/reducers/systemLogReducer.ts
+++ src/store/reducers/systemLogReducer.ts
@@ -1,27 +1,39 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+import ISystemLogState from '../../interfaces/ISystemLogState';
 import ISystemLog from '../../interfaces/ISystemLog';
+import { RootState } from '../index';
 
-const initialState: Array<ISystemLog> = [{
-  id: 0,
-  attention: false,
-  date: "",
-  message: "",
-  tech: "",
-}]
+const initialState: ISystemLogState = {
+  loading: false,
+  logs: [],
+}
 
 export const systemLogReducer = createSlice({
   name: 'systemLogs',
   initialState,
   reducers: {
-    getLogs: state => state,
-    addLog: (state, action: PayloadAction<ISystemLog>) => [...state, action.payload],
-    deleteLog: (state, action: PayloadAction<number>) => state.filter(log => log.id != action.payload),
-    getLogById: (state, action: PayloadAction<number>) => state.filter(log => log.id == action.payload),
-    filterLogsByTech: (state, action: PayloadAction<string>) => state.filter(log => log.tech == action.payload),
+    addLog: (state, action: PayloadAction<ISystemLog>) => {
+      state.logs.push(action.payload)
+    },
+    updateLog: (state, action: PayloadAction<ISystemLog>) => {
+      const logToUpdateIndex = state.logs.findIndex(log => log.id == action.payload.id);
+      state.logs[logToUpdateIndex] = action.payload;
+    },
+    deleteLog: (state, action: PayloadAction<number>) => {
+      state.logs = state.logs.filter(log => log.id != action.payload);
+    },
   }
 });
 
-export const { getLogs } = systemLogReducer.actions;
+export const { deleteLog, addLog } = systemLogReducer.actions;
+export const getLogs = (state: RootState) => state.systemLogReducer.logs;
+export const getLogById = (state: RootState, action: PayloadAction<number>) => {
+  return state.systemLogReducer.logs.find(log => log.id == action.payload);
+}
+export const filterLogsByTech = (state: RootState, action: PayloadAction<string>) => {
+  return state.systemLogReducer.logs.filter(log => log.tech == action.payload);
+}
+export const isLoadng = (state: RootState) => state.systemLogReducer.loading;
 
 export default systemLogReducer.reducer;

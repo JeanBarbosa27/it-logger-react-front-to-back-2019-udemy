9d31b82c37b26b2aca8913653d1b24be0d4fb4b3
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-07 14:16:09 +0100

    feat: add system log DTO interface

--- src/interfaces/ISystemLog.ts
+++ src/interfaces/ISystemLog.ts
@@ -1,9 +1,12 @@
-interface ISystemLog {
+export interface ISystemLogDTO {
   attention: boolean;
   date: string;
-  id: number;
   message: string;
   tech: string;
+}
+
+interface ISystemLog extends ISystemLogDTO {
+  id: number;
 }
 
 export default ISystemLog;

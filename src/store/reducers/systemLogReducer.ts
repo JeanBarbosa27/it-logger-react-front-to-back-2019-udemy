9bd4de2386e2bea9e4c18f8cd86c113ca50df3c3
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-07 12:10:46 +0100

    feat: add fetchLogs async thunk action
    This action is required to get all logs from back-end and have it in the state. Add some refactoring due to code organization and decoupling.

--- src/store/reducers/systemLogReducer.ts
+++ src/store/reducers/systemLogReducer.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 import ISystemLogState from '../../interfaces/ISystemLogState';
 import ISystemLog from '../../interfaces/ISystemLog';
@@ -12,16 +12,39 @@ const initialState: ISystemLogState = {
   logs: null,
 }
 
+// Selectors
+export const getLogs = (state: RootState) => state.systemLog.logs;
+export const getLogById = (state: RootState, action: PayloadAction<number>) => {
+  return state.systemLog.logs?.find(log => log.id == action.payload);
+}
+export const filterLogsByTech = (state: RootState, action: PayloadAction<string>) => {
+  return state.systemLog.logs?.filter(log => log.tech == action.payload);
+}
+export const isFirstLoad = (state: RootState) => state.systemLog.firstLoad;
+export const isLoading = (state: RootState) => state.systemLog.loading;
+
+// Async Thunk actions
+export const fetchLogs = createAsyncThunk(
+  'systemLog/fetchLogs',
+  async () => {
+    const response = await fetch('http://localhost:5000/logs');
+
+    return (await response.json()) as Array<ISystemLog>;
+  }
+);
+
+// Reducer slices
 export const systemLogReducer = createSlice({
   name: 'systemLog',
   initialState,
   reducers: {
-    addLog: (state, action: PayloadAction<ISystemLog>) => {
+    addLog(state, action: PayloadAction<ISystemLog>) {
       state.logs?.push(action.payload);
     },
-    addLogsInBatch: (state, action: PayloadAction<Array<ISystemLog>>) => {
+    addLogsInBatch(state, action: PayloadAction<Array<ISystemLog>>) {
       if (state.firstLoad) {
         state.logs = action.payload;
+        setFirstLoad(false);
 
         return;
       }
@@ -32,30 +55,44 @@ export const systemLogReducer = createSlice({
 
       action.payload.forEach(log => state.logs?.push(log))
     },
-    updateLog: (state, action: PayloadAction<ISystemLog>) => {
+    updateLog(state, action: PayloadAction<ISystemLog>) {
       state.logs?.forEach(log => {
         if (log.id == action.payload.id) {
           log = action.payload;
         }
       })
     },
-    deleteLog: (state, action: PayloadAction<number>) => {
+    deleteLog(state, action: PayloadAction<number>) {
       if (state.logs) {
         state.logs = state.logs.filter(log => log.id != action.payload);
       }
     },
-    setError: (state, action: PayloadAction<string>) => {
+    setError(state, action: PayloadAction<string>) {
       state.error = action.payload;
     },
-    setFirstLoad: (state, action: PayloadAction<boolean>) => {
+    setFirstLoad(state, action: PayloadAction<boolean>) {
       state.firstLoad = action.payload;
     },
-    setLoading: (state, action: PayloadAction<boolean>) => {
+    setLoading(state, action: PayloadAction<boolean>) {
       state.loading = action.payload
     },
-    clearLogs: state => {
+    clearLogs(state) {
       state = initialState
     },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchLogs.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchLogs.fulfilled, (state, action) => {
+        state.loading = false;
+        state.logs = action.payload;
+      })
+      .addCase(fetchLogs.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      })
   }
 });
 
@@ -69,14 +106,5 @@ export const {
   setLoading,
   clearLogs
 } = systemLogReducer.actions;
-export const getLogs = (state: RootState) => state.systemLog.logs;
-export const getLogById = (state: RootState, action: PayloadAction<number>) => {
-  return state.systemLog.logs?.find(log => log.id == action.payload);
-}
-export const filterLogsByTech = (state: RootState, action: PayloadAction<string>) => {
-  return state.systemLog.logs?.filter(log => log.tech == action.payload);
-}
-export const isFirstLoad = (state: RootState) => state.systemLog.firstLoad;
-export const isLoading = (state: RootState) => state.systemLog.loading;
 
 export default systemLogReducer.reducer;

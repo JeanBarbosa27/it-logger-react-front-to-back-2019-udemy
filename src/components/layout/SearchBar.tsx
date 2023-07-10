76594604f8ebf4268d0cb85bb200ef6bcc5503dc
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-10 13:58:49 +0100

    feat: Search logs by message or technician
    The search part is followed as it is in the course, the debounce part I searched for it by my self and adapted to TypeScrit.

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+
+import { useAppDispatch } from '../../store/hooks';
+import { searchLog } from '../../store/reducers/systemLogReducer';
+import useDebounce from '../../utils/hooks/useDebounce';
+
 const SearchBar = () => {
+  const dispatch = useAppDispatch();
+  const [text, setText] = useState('');
+  const debounceSearch = useDebounce(() => {
+    dispatch(searchLog(text));
+  });
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.currentTarget.value)
+
+    debounceSearch();
+  }
+
   return (
     <nav style={{ marginBottom: '30px' }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" />
+            <input
+              id="search"
+              type="search"
+              placeholder='Search logs...'
+              value={text}
+              onChange={onChange}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>

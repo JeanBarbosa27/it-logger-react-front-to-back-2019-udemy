a9f3e22d54599b526f993cabf13396357efd5370
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-11 14:02:02 +0100

    docs: add notes and commet to future reference

--- src/utils/timers.ts
+++ src/utils/timers.ts
@@ -1,3 +1,4 @@
+// Considers only the last sent
 export const debounce = (callback: Function, wait = 500) => {
   let timer: ReturnType<typeof setTimeout>;
 
@@ -6,3 +7,7 @@ export const debounce = (callback: Function, wait = 500) => {
     timer = setTimeout(() => callback.apply(this, args), wait);
   }
 }
+
+// TODO: Debounce that considers only the first one to avoid multiple clicks, for example
+
+// TODO: Throttle to save changes on large fields such as description, e.g., to save each Xms as user types in it

8d2457ae33889cfef63c083650460895e2a4295b
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-11 09:30:30 +0100

    feat: add DTO for tech interface

--- src/interfaces/ITech.ts
+++ src/interfaces/ITech.ts
@@ -1,7 +1,8 @@
-interface ITech {
-  id: number;
+export interface ITechDTO {
   firstName: string;
   lastName: string;
 }
 
-export default ITech;
+export default interface ITech extends ITechDTO {
+  id: number;
+}

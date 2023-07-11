f748f945b7eccc10a1031b7899e5c37a40e08272
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-11 09:33:54 +0100

    feat: delete tech by trash click on techs modal

--- src/components/techs/TechsItem.tsx
+++ src/components/techs/TechsItem.tsx
@@ -1,4 +1,8 @@
+import materialize from 'materialize-css/dist/js/materialize.min.js';
+
 import ITech from '../../interfaces/ITech';
+import { useAppDispatch } from '../../store/hooks';
+import { deleteTech } from '../../store/reducers/techReducers';
 
 interface ITechItemProps {
   tech: ITech;
@@ -7,10 +11,16 @@ interface ITechItemProps {
 // TODO: Should be interesting be able to edit the technician just click on its name, right on the item, without close this and opening a new modal. It should change between two inputs and the full name text
 
 const TechsItem = ({ tech }: ITechItemProps) => {
+  const dispatch = useAppDispatch();
+
+  const onDelete = async () => {
+    await dispatch(deleteTech(tech.id));
+    materialize.toast({ html: `Technician ${tech.firstName} ${tech.lastName} deleted!` });
+  }
   return (
     <div>
       {tech.firstName} {tech.lastName}
-      <a href="#!" className="secondary-content grey-text">
+      <a href="#!" className="secondary-content grey-text" onClick={onDelete}>
         <i className="material-icons delete-button">delete</i>
       </a>
     </div>

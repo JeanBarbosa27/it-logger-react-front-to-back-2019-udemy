5e420033d7acaf0797e6cf1c3cd312188c2d4782
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-14 08:49:45 +0100

    feat: add tech model
    It will work as model and repository, since it has functions to do the CRUD

---
+++ server/src/models/TechModel.ts
@@ -0,0 +1,25 @@
+import { Model, Schema, model } from 'mongoose';
+
+interface ITech {
+  firstName: string;
+  lastName: string;
+}
+
+interface ITechMethods {
+  fullName(): string;
+}
+
+type ITechModel = Model<ITech, {}, ITechMethods>;
+
+const techSchema = new Schema<ITech, ITechModel, ITechMethods>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+techSchema.method('fullName', function fullName() {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+const techModel = model<ITech, ITechModel>('Tech', techSchema);
+
+export default techModel

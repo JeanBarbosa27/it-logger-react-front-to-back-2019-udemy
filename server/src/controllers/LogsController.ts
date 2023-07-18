5f59bb0f9cc3f599ac9b271a695b61a6e683e64c
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-18 09:13:54 +0100

    feat: get all logs

--- server/src/controllers/LogsController.ts
+++ server/src/controllers/LogsController.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from 'express';
 
 import ICotronller from './IController';
+import { TLogModel } from '../models/LogModel';
 
 export default class LogsController implements ICotronller {
+  private model: TLogModel;
+
+  constructor(model: TLogModel) {
+    this.model = model;
+  }
+
   public get = async (request: Request, response: Response) => {
-    // TODO: implement logic
-    return response.send({})
+    const allLogs = await this.model.find();
+
+    return response.send(allLogs);
   }
 
   public post = async (request: Request, response: Response) => {

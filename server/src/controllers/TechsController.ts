9b3aa4e8b93ee4b0a4b54580caf6016d72df52bb
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-18 09:14:22 +0100

    feat: get all techs

--- server/src/controllers/TechsController.ts
+++ server/src/controllers/TechsController.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
 
 import ICotronller from './IController';
+import { TTechModel } from 'models/TechModel';
 export default class TechsController implements ICotronller {
+  private model: TTechModel;
+
+  constructor(model: TTechModel) {
+    this.model = model;
+  }
+
   public get = async (request: Request, response: Response) => {
-    // TODO: Implement logic
-    return response.send({});
+    const allTechs = await this.model.find();
+
+    return response.send(allTechs);
   }
 
   public post = async (request: Request, response: Response) => {

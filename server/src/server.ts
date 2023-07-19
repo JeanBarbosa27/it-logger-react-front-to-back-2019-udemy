ec8364ac4bde7820c58a728ff8ed9e2c2d575cd6
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-19 09:39:21 +0100

    feat: add middleware non handled route error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Express, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 import ConnectMongoDB from './db/ConnectMongoDB.js';
@@ -14,7 +14,7 @@ const app: Express = express();
 const environment = process.env.ENVIRONMENT
 const port = process.env.PORT;
 
-const appRoutes = new AppRoutes().getRoutes();
+const { getRoutes, getAvailableRoutes } = new AppRoutes();
 const connectMongoDB = new ConnectMongoDB(process.env.MONGO_DB_URI);
 connectMongoDB.connect();
 
@@ -24,7 +24,12 @@ app.get('/health-check', (_, response: Response) => {
   response.send('Ok');
 });
 
-app.use(appRoutes);
+app.use(getRoutes());
+
+app.use((request: Request, response: Response, next: NextFunction) => {
+  const availableRoutes = getAvailableRoutes().map(({ route }) => route);
+  response.status(404).json({ errors: [`Endpoint not found, available endpoints are ${availableRoutes.join(', ')}`] });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${environment === 'development' ? 'http://localhost:' + port : port}`);

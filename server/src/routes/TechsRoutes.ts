2c844e9e97936072d110d697b0883cded3af2f4c
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-07-19 09:37:27 +0100

    feat: add index route and validation middleware

--- server/src/routes/TechsRoutes.ts
+++ server/src/routes/TechsRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import IRoutes from './IRoutes.js';
 import TechsController from '../controllers/TechsController.js';
+import TechsMiddlewares from '../middlewares/TechsMiddlewares.js';
 import techModel from '../models/TechModel.js';
 
 export default class TechsRoutes implements IRoutes {
@@ -13,8 +14,9 @@ export default class TechsRoutes implements IRoutes {
   }
 
   public getRoutes = () => {
-    this.router.get('/', this.controller.get);
-    this.router.post('/', this.controller.post);
+    this.router.get('/', this.controller.index);
+    this.router.get('/:id', this.controller.get);
+    this.router.post('/', TechsMiddlewares.validateTechCreation, this.controller.post);
     this.router.put('/:id', this.controller.put);
     this.router.delete('/:id', this.controller.delete);
 
